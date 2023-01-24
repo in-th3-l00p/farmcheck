5b3cc4c5bac9cfdc9806eb26cd4062cc260431e5
intheloop <indianucode0303@gmail.com>
2023-01-24 20:43:26 +0200

    siiii x2

--- src/main/java/com/cybertech/farmcheck/domain/User.java
+++ src/main/java/com/cybertech/farmcheck/domain/User.java
@@ -59,7 +59,7 @@ public class User extends AbstractAuditingEntity implements Serializable {
 
     @NotNull
     @Column(nullable = false)
-    private boolean activated = true;
+    private boolean activated = false;
 
     @Size(min = 2, max = 10)
     @Column(name = "lang_key", length = 10)
